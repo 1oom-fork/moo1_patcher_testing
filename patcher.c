ec0cbe572b812fd57bfd55885d6aa350160f9fa8
MyName <fork1oom@yandex.com>
2024-03-09 16:50:07 +0300

    Fix feof/ferror checks

--- patcher.c
+++ patcher.c
@@ -4,12 +4,16 @@
 int moobin_check(FILE *f, int offset, const uint8_t str[], int len)
 {
     fseek(f, offset, SEEK_SET);
+    if (feof(f) || ferror(f)) {
+        printf("feof/ferror\n");
+        return -1;
+    }
     for (int i = 0; i < len; ++i) {
+        uint8_t c = getc(f) & 0xff;
         if (feof(f) || ferror(f)) {
             printf("feof/ferror\n");
             return -1;
         }
-        uint8_t c = getc(f) & 0xff;
         if (c != str[i]) {
             return -2;
         }
@@ -20,12 +24,16 @@ int moobin_check(FILE *f, int offset, const uint8_t str[], int len)
 int moobin_check_nop(FILE *f, int offset, int len)
 {
     fseek(f, offset, SEEK_SET);
+    if (feof(f) || ferror(f)) {
+        printf("feof/ferror\n");
+        return -1;
+    }
     for (int i = 0; i < len; ++i) {
+        uint8_t c = getc(f) & 0xff;
         if (feof(f) || ferror(f)) {
             printf("feof/ferror\n");
             return -1;
         }
-        uint8_t c = getc(f) & 0xff;
         if (c != 0x90) {
             return -2;
         }
@@ -36,12 +44,16 @@ int moobin_check_nop(FILE *f, int offset, int len)
 int moobin_set_nop(FILE *f, int offset, int len)
 {
     fseek(f, offset, SEEK_SET);
+    if (feof(f) || ferror(f)) {
+        printf("feof/ferror\n");
+        return -1;
+    }
     for (int i = 0; i < len; ++i) {
+        putc(0x90, f);
         if (feof(f) || ferror(f)) {
             printf("feof/ferror\n");
             return -1;
         }
-        putc(0x90, f);
     }
     return 0;
 }
@@ -49,12 +61,16 @@ int moobin_set_nop(FILE *f, int offset, int len)
 int moobin_replace(FILE *f, int offset, const uint8_t str[], int len)
 {
     fseek(f, offset, SEEK_SET);
+    if (feof(f) || ferror(f)) {
+        printf("feof/ferror\n");
+        return -1;
+    }
     for (int i = 0; i < len; ++i) {
+        putc(str[i], f);
         if (feof(f) || ferror(f)) {
             printf("feof/ferror\n");
             return -1;
         }
-        putc(str[i], f);
     }
     return 0;
 }
